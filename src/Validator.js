d5c717d492a339cc0da8b5d663d912f9332d3de6
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-03-03 21:14:05 -0500

    done

--- src/Validator.js
+++ src/Validator.js
@@ -2,12 +2,51 @@ import React, { Component } from 'react';
 import './Validator.css';
 
 class Validator extends Component {
+
+  constructor() {
+    super()
+    this.handlePassword = this.handlePassword.bind(this)
+    this.handleConfirm = this.handleConfirm.bind(this)
+    this.handleMatch = this.handleMatch.bind(this)
+    this.state = {
+      email: "",
+      password: "",
+      passwordConfirm: "",
+      valid: ""
+    };
+  }
+
+  handlePassword = (e)=> {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  handleConfirm = (e)=> {
+    this.setState({
+      passwordConfirm: e.target.value
+    })
+  }
+
+  handleMatch = ()=> {
+    if (this.state.password === this.state.passwordConfirm) {
+      this.setState({valid: 'Passwords Match BRO!'})
+    } else if (this.state.password !== this.state.passwordConfirm) {
+      this.setState({valid: 'Passwords do not match bro'})
+    }
+
+  }
+
   render() {
     return (
       <div className="form">
         <h1>Sign Up</h1>
         <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
+        <input type="email" placeholder="email" />
+        <input type="password" onChange = {this.handlePassword} placeholder="password" />
+        <input type="password" onChange = {this.handleConfirm} placeholder="confirm password" />
+        <button onClick={this.handleMatch} >Submit</button>
+        <div>{this.state.valid}</div>
       </div>
     );
   }
